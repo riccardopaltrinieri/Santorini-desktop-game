aef7a42baf57d229b3d210db46445aa6f38162f9
ilpaliuz <61739098+ilpaliuz@users.noreply.github.com>
2020-03-30 17:11:40 +0200

    Tab e Cell class

--- src/main/java/it/polimi/ingsw/Model/Cell.java
+++ src/main/java/it/polimi/ingsw/Model/Cell.java
@@ -1,4 +1,24 @@
 package it.polimi.ingsw.Model;
 
 public class Cell {
+    private int numRow;
+    private int numColumn;
+    private int level;
+    private boolean isEmpty;
+
+
+    public int getNumRow(Cell cell){
+        return cell.numRow;
+    }
+    public int getNumColumn(Cell cell){
+        return cell.numColumn;
+    }
+    public int getLevel(Cell cell){
+        return cell.level;
+    }
+    public boolean isEmpty(Cell cell){
+        return cell.isEmpty;
+    }
+
+
 }
--- src/main/java/it/polimi/ingsw/Model/Tab.java
+++ src/main/java/it/polimi/ingsw/Model/Tab.java
@@ -1,4 +1,21 @@
 package it.polimi.ingsw.Model;
 
 public class Tab {
-}
+    private int numRow=5;
+    private int numColumn=5;
+
+
+    public int getNumRow() {
+        return numRow;
+    }
+
+    public int getNumColumn() {
+        return numColumn;
+    }
+
+
+     public void clear(){
+         }
+     }
+
+
