42fdab3a7f406dd4163f0d5365965170f74e13e3
filloPagliani <61702711+filloPagliani@users.noreply.github.com>
2020-03-30 16:13:18 +0200

    aggiunti attributi di Worker e Turn

--- src/main/java/it/polimi/ingsw/Model/Turn.java
+++ src/main/java/it/polimi/ingsw/Model/Turn.java
@@ -1,4 +1,34 @@
 package it.polimi.ingsw.Model;
 
 public class Turn {
+    //Attributi
+    private int number;
+    private Divinity godName;
+    private GodPower godPower;
+
+    //getter e setter
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Divinity getGodName() {
+        return godName;
+    }
+
+    public void setGodName(Divinity godName) {
+        this.godName = godName;
+    }
+
+    public GodPower getGodPower() {
+        return godPower;
+    }
+
+    public void setGodPower(GodPower godPower) {
+        this.godPower = godPower;
+    }
 }
--- src/main/java/it/polimi/ingsw/Model/Worker.java
+++ src/main/java/it/polimi/ingsw/Model/Worker.java
@@ -1,4 +1,26 @@
 package it.polimi.ingsw.Model;
 
 public class Worker {
+
+    //Attributi
+    private Cell position;
+    private Player owner;
+
+    //getter e setter
+
+    public Cell getPosition() {
+        return position;
+    }
+
+    public void setPosition(Cell position) {
+        this.position = position;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Player owner) {
+        this.owner = owner;
+    }
 }
