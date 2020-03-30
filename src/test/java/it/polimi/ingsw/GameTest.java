133783ca0b98cc53f8fc6eb60f1a352d18514153
Riccardo Paltrinieri <61794499+riccardopaltrinieri@users.noreply.github.com>
2020-03-30 13:04:54 +0200

    Added directory layout and example classes

---
+++ src/test/java/it/polimi/ingsw/GameTest.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+
+public class GameTest
+{
+    @Test
+    public void Test()
+    {
+        assertTrue( true );
+    }
+}
