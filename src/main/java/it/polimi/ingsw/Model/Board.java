e7431d3555b5f075325e5fd516e2fa2a279f14cb
ilpaliuz <61739098+ilpaliuz@users.noreply.github.com>
2020-04-02 20:08:13 +0200

    Costruttore Tab (sotto commento,non finito)

--- src/main/java/it/polimi/ingsw/Model/Board.java
+++ src/main/java/it/polimi/ingsw/Model/Board.java
@@ -1,23 +1,24 @@
 package it.polimi.ingsw.Model;
 
 public class Board {
-    private int numRow=5;
-    private int numColumn=5;
-private Cell[][] map;
+    private int numRow;
+    private int numColumn;
+public Cell[][] map;
 
     public Board () {
         int i;
         int j;
-
-        for (i = 0; i < 5; i++) {
+        this.numRow=5;
+        this.numColumn=5;
+      /*  Cell[][] map=new Cell[5][5];
+       for (i = 0; i < 5; i++) {
             for (j = 0; j < 5; j++) {
                 this.map[i][j].setEmptyDefault();
                 this.map[i][j].setNumRow(i);
                 this.map[i][j].setNumColumn(j);
-            }
-        }
-       // System.out.println(this.map[1][1].getNumRow());
-    }
+       } */
+
+}
 
 
     public int getNumRow() {
--- src/test/java/it/polimi/ingsw/BoardTest.java
+++ src/test/java/it/polimi/ingsw/BoardTest.java
@@ -21,5 +21,7 @@ public class BoardTest {
 
 
 
+
+
     }
 }
