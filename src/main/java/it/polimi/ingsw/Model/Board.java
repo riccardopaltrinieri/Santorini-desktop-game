91b52ba519cf18f4b8b0d17b80d27a2f60f24447
ilpaliuz <61739098+ilpaliuz@users.noreply.github.com>
2020-04-02 20:42:32 +0200

    Costruttore Tab e TestTab

--- src/main/java/it/polimi/ingsw/Model/Board.java
+++ src/main/java/it/polimi/ingsw/Model/Board.java
@@ -1,25 +1,24 @@
 package it.polimi.ingsw.Model;
 
 public class Board {
-    private int numRow;
-    private int numColumn;
+    private int numRow=5;
+    private int numColumn=5;
 public Cell[][] map;
 
     public Board () {
         int i;
         int j;
-        this.numRow=5;
-        this.numColumn=5;
-      /*  Cell[][] map=new Cell[5][5];
-       for (i = 0; i < 5; i++) {
+        map = new Cell[5][5];
+        for (i = 0; i < 5; i++) {
             for (j = 0; j < 5; j++) {
-                this.map[i][j].setEmptyDefault();
+                this.map[i][j]=new Cell();
                 this.map[i][j].setNumRow(i);
                 this.map[i][j].setNumColumn(j);
-       } */
+            }
 
-}
+        }
 
+    }
 
     public int getNumRow() {
         return numRow;
