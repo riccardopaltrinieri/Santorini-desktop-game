702112f88bba14803cff9a02f38a7e946969a7f3
ilpaliuz <61739098+ilpaliuz@users.noreply.github.com>
2020-05-12 17:26:53 +0200

    LiteBoard

--- src/main/java/it/polimi/ingsw/Model/Cell.java
+++ src/main/java/it/polimi/ingsw/Model/Cell.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.Model;
 
-public class Cell {
+import java.io.Serializable;
+
+public class Cell implements Serializable {
     private int numRow;
     private int numColumn;
     private int level;
@@ -12,7 +14,7 @@ public class Cell {
      * @param column is the column where you want to place the cell
      * @throws IllegalArgumentException if you are trying to create a cell outside of the 5x5 tab
      */
-    public Cell (int row, int column) throws IllegalArgumentException{
+    public Cell (int row, int column) throws IllegalArgumentException {
         if(row<5&&column<5) {
             this.numColumn = column;
             this.numRow = row;
---
+++ src/main/java/it/polimi/ingsw/View/LiteBoard.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.View;
+
+import it.polimi.ingsw.Model.Cell;
+import it.polimi.ingsw.Model.Worker;
+
+public class LiteBoard {
+    private int[][] lv; // [riga][colonna] = livello
+    private int[][] posworker; // [worker] [0] = riga
+                                //[worker] [1] = colonna
+                                //[worker] [2] = COLORE (USATE PAINT )
+    private String comand;
+
+}
