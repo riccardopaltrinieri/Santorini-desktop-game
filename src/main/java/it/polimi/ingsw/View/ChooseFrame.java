a19b1309a7f8a910226f26389ce82a77282f0d8a
filloPagliani <61702711+filloPagliani@users.noreply.github.com>
2020-05-17 13:25:37 +0200

    ho aggiunto due righe ma ora devo andare a pranzo e vanno comunque salvate

---
+++ src/main/java/it/polimi/ingsw/View/ChooseFrame.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.View;
+
+import it.polimi.ingsw.Model.Divinity;
+
+import javax.swing.*;
+
+public class ChooseFrame {
+
+    private JPanel godPanel = new JPanel();
+    private JPanel westerPanel = new JPanel();
+
+    private JTextArea infoText = new JTextArea();
+
+    private Icon godIcon;
+
+    private JButton selectButton = new JButton("Select");
+
+    private JComboBox divinityList;
+}
