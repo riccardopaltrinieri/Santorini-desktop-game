4ae2d564361e4d70f19bef340da779f9647795cf
filloPagliani <61702711+filloPagliani@users.noreply.github.com>
2020-05-25 13:17:25 +0200

    added panTest class

---
+++ src/test/java/it/polimi/ingsw/Model/Divinities/PanTest.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.Model.Divinities;
+
+import it.polimi.ingsw.AthenaException;
+import it.polimi.ingsw.Model.*;
+import org.junit.Test;
+
+public class PanTest {
+    GodPower test = new Pan();
+
+    @Test
+    public void panTest()
+    {
+        Game game = new Game();
+        Player panPlayer = new Player("pan's player", Color.Green, game);
+        Board board = game.getBoard();
+        panPlayer.placeWorkers(board.getCell(1,1));
+        board.getCell(1,1).setLevel(2);
+        panPlayer.setGodPower(test);
+        try{
+            panPlayer.getWorker(0).move(board.getCell(2,2));
+        }
+        catch (AthenaException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
